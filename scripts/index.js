03f2324b9bf12570c36ef0ee83c28ed29da62aa3
VIP-ИГРОК <free2fire12viptt@gmail.com>
2024-08-02 10:16:53 +0600

    add new function map in js

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,46 @@ for(let i = 0; i < links.length; i++ ) {
         .scrollIntoView({ behavior: "smooth"});
     }
 }
+
+const products = [
+    {
+        id: 1,
+        name: "Cosmetic products",
+        descrtion: "Lorem Ipsum - это просто фиктивный текст для полиграфической промышленности. Lorem Ipsum был стандартным фиктивным текстом в отрасли.",
+        image: "./assets/image/products1.png",
+        price: "120",
+    },
+    {
+        id: 2,
+        name: "Cosmetic products",
+        descrtion: "Lorem Ipsum - это просто фиктивный текст для полиграфической промышленности. Lorem Ipsum был стандартным фиктивным текстом в отрасли.",
+        image: "./assets/image/products2.png",
+        price: "120",
+    },
+];
+
+const productsBox = document.querySelector(".products-box");
+const productsMap = products.map((el) => {
+    return `
+     <div class="card">
+                <img src="${el.image}" alt="" loading="lazy"/>
+                <div class="card-content">
+                  <h2>${el.name}</h2>
+                <p>
+                ${el.descrtion}
+                </p>
+                <div class="card-actions">
+                  <div class="card-action">
+                    <button>Buy Now</button>
+                    <button>Add To cart</button>
+                  </div>
+                  <span>${el.price} $</span>
+                </div>
+                </div>
+              </div>
+            </div>
+    `
+});
+
+productsBox.innerHTML = productsMap.join("")
+console.log(productsMap)
